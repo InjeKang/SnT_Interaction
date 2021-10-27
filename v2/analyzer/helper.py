cff7dcea81d86b6fde9b66c639ff29c2561dc0f4
InjeKang <58596670+InjeKang@users.noreply.github.com>
2021-10-27 18:09:18 +0900

    Delete v2 directory

--- v2/analyzer/helper.py
+++
@@ -1,9 +0,0 @@
-def create_breakpoints(start_year, end_year, max_month):
-    years = list(range(start_year, end_year+1))
-    months = ["0{}".format(i) if i < 10 else "{}".format(i) for i in range(1, 13)]
-    year_month = ["{}{}".format(year, month)
-                    for year in years for month in months
-                    if int("{}{}".format(year, month)) <= max_month]
-    year_month = pd.Series(year_month)
-    year_month = year_month.map(lambda x: dt.datetime.strptime(x, "%Y%m"))
-    return year_month
